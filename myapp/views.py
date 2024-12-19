b2e68dffade778c4d4a37ff558096385e0470cc9
sis-I <sisshash@gmail.com>
2024-12-19 10:54:59 +0300

    add entory in people table and modify views

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import People
+
 # Create your views here.
 def home(request):
+
+    person = People.objects.get(name="this is my name")
     html = f'''
         <h1>Home</h1>
         <div style="margin-top: 40px;">
             This page is home page!!
+            {person.name}
         </div>
     '''
 
