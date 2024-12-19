71ffcdd98138e7d7a83213ec50a48b0d00923591
sis-I <sisshash@gmail.com>
2024-12-19 11:03:40 +0300

    install psycopg2-binary and modify settings.py file

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -82,7 +82,7 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2_binary',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
