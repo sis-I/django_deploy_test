6bf8bf2f6b7fcd09d39776ae0e916736332f95ba
sis-I <sisshash@gmail.com>
2024-12-19 11:29:03 +0300

    modified settings

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -82,7 +82,7 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2_binary',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
